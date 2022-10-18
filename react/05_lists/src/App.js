78122ea50183dbf578c754c0071a45d72dd6b5c2
Erno Hentonen <erno.hentonen@opiframe.com>
2022-10-18 10:41:20 +0300

    lists

--- react/05_lists/src/App.js
+++ react/05_lists/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
 
 function App() {
 	
@@ -33,6 +34,8 @@ function App() {
 	return (
 		<div className="App">
 			<ContactForm addContact={addContact}/>
+			<hr/>
+			<ContactList list={state.list} removeContact={removeContact}/>
 		</div>
 	);
 }
