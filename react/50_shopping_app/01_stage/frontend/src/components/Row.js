2badc102a7873f9bc31c9d31d4d125ace5e58e80
Erno Hentonen <erno.hentonen@opiframe.com>
2022-10-19 14:01:35 +0300

    frontend stage 1

--- react/50_shopping_app/01_stage/frontend/src/components/Row.js
+++ react/50_shopping_app/01_stage/frontend/src/components/Row.js
@@ -6,10 +6,10 @@ const Row = (props) => {
 			<td>{props.item.count}</td>
 			<td>{props.item.price}</td>
 			<td><button 
-			onClick={() => props.changeMode("remove",index)}
+			onClick={() => props.changeMode("remove",props.index)}
 			className="btn btn-danger">Remove</button></td>
 			<td><button 
-			onClick={() => props.changeMode("edit",index)}
+			onClick={() => props.changeMode("edit",props.index)}
 			className="btn btn-secondary">Edit</button></td>
 		</tr>
 	)
