56773a0c365b186b85cb2c77c16cd3cc8fa44491
Erno Hentonen <erno.hentonen@opiframe.com>
2022-10-24 10:53:00 +0300

    stage 2

--- react/50_shopping_app/02_stage_login/backend/server.js
+++ react/50_shopping_app/02_stage_login/backend/server.js
@@ -1,9 +1,46 @@
 const express = require("express");
 const apiroute = require("./routes/apiroute")
+const bcrypt = require("bcrypt");
 
 let app = express();
 
 app.use(express.json());
+
+//LOGIN DATABASES
+
+const registeredUsers = [];
+const loggedSessions = [];
+
+//LOGIN API
+
+app.post("/register",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({message:"Bad Request"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message:"Bad Request"});
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({message:"Bad Request"});
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(req.body.username === registeredUsers[i].username) {
+			return res.status(409).json({message:"Username is already in use"})
+		}
+	}
+	bcrypt.hash(req.body.password,14,function(err,hash) {
+		if(err) {
+			return res.status(500).json({message:"Internal server error"})
+		}
+		let user = {
+			username:req.body.username,
+			password:hash
+		}
+		registeredUsers.push(user);
+		console.log(user);
+		return res.status(201).json({message:"Register success"});
+	})
+})
 
 let port = process.env.PORT || 3001;
 
