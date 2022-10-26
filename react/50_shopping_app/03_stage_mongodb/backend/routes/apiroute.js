42ec7c1aec1e899fbfb88509e7787cd3ae13b3b2
Erno Hentonen <erno.hentonen@opiframe.com>
2022-10-26 09:31:50 +0300

    stage 3

--- react/50_shopping_app/03_stage_mongodb/backend/routes/apiroute.js
+++ react/50_shopping_app/03_stage_mongodb/backend/routes/apiroute.js
@@ -44,36 +44,35 @@ router.post("/shopping",function(req,res) {
 });
 
 router.delete("/shopping/:id",function(req,res) {
-	let tempId = parseInt(req.params.id)
-	for(let i=0;i<database.length;i++) {
-		if(database[i].id === tempId) {
-			if(database[i].user !== req.session.user) {
-				return res.status(404).json({message:"Not found"})
-			}
-			database.splice(i,1);
-			return res.status(200).json({message:"Success"})
+	itemModel.deleteOne({"_id":req.params.id,"user":req.session.user},function(err) {
+		if(err) {
+			console.log("Failed to remove item. Reason",err);
+			return res.status(500).json({message:"Internal server error"})
 		}
-	}
-	return res.status(404).json({message:"Not found"});
+		return res.status(200).json({message:"Success"});
+	})
 })
 
 router.put("/shopping/:id",function(req,res) {
-	let tempId = parseInt(req.params.id);
+	if(!req.body) {
+		return res.status(400).json({message:"Bad Request"})
+	}
+	if(!req.body.type) {
+		return res.status(400).json({message:"Bad Request"})
+	}
 	let item = {
-		...req.body,
-		id:tempId,
+		type:req.body.type,
+		count:req.body.count,
+		price:req.body.price,
 		user:req.session.user
 	}
-	for(let i=0;i<database.length;i++) {
-		if(database[i].id === tempId) {
-			if(database[i].user !== req.session.user) {
-				return res.status(404).json({message:"Not found"})
-			}
-			database.splice(i,1,item);
-			return res.status(200).json({message:"Success"});
+	itemModel.replaceOne({"_id":req.params.id,"user":req.session.user},item,function(err) {
+		if(err) {
+			console.log("Failed to update item. Reason",err);
+			return res.status(500).json({message:"Internal server error"})
 		}
-	}
-	return res.status(404).json({message:"Not found"});
+		return res.status(200).json({message:"Success"});
+	})
 })
 
 module.exports = router;
