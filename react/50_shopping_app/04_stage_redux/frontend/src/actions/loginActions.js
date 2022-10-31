e2b6246eb826964b0bced853ee5dfd58aa8b905a
Erno Hentonen <erno.hentonen@opiframe.com>
2022-10-31 11:01:06 +0200

    loginreducer

--- react/50_shopping_app/04_stage_redux/frontend/src/actions/loginActions.js
+++ react/50_shopping_app/04_stage_redux/frontend/src/actions/loginActions.js
@@ -2,6 +2,31 @@ import * as actionConstants from './actionConstants';
 
 //ASYNC ACTION CREATORS
 
+export const register = (user) => {
+	return async (dispatch) => {
+		let request = {
+			method:"POST",
+			headers:{"Content-Type":"application/json"},
+			body:JSON.stringify(user)
+		}
+		dispatch(loading());
+		let response = await fetch("/register",request);
+		dispatch(stopLoading());
+		if(!response) {
+			dispatch(registerFailed("Communication error with the server. Retry later."));
+			return;
+		}
+		if(response.ok) {
+			dispatch(registerSuccess());
+		} else {
+			if(response.status === 409) {
+				dispatch(registerFailed("Username already in use"));
+				return;
+			}
+			dispatch(registerFailed("Register failed. Server responded with a status "+response.status+" "+response.statusText));
+		}
+	}
+}
 
 //ACTION CREATORS
 
