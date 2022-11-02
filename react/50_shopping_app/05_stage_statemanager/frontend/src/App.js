8b419ef00c5dafc391b66db196f07c7afda81183
Erno Hentonen <erno.hentonen@opiframe.com>
2022-11-02 10:00:18 +0200

    statemanager

--- react/50_shopping_app/05_stage_statemanager/frontend/src/App.js
+++ react/50_shopping_app/05_stage_statemanager/frontend/src/App.js
@@ -1,4 +1,6 @@
-import {useState,useEffect} from 'react';
+import {useEffect} from 'react';
+import useAppState from './hooks/useAppState';
+import useAction from './hooks/useAction';
 import './App.css';
 import ShoppingForm from './components/ShoppingForm';
 import ShoppingList from './components/ShoppingList';
@@ -8,295 +10,38 @@ import {Routes,Route,Navigate} from 'react-router-dom';
 
 function App() {
 	
-	const [state,setState] = useState({
-		list:[],
-		token:"",
-		isLogged:false,
-		error:"",
-		loading:false
-	})
-	
-	const [urlRequest,setUrlRequest] = useState({
-		url:"",
-		request:{},
-		action:""
-	})
-	
-	//STORAGE FUNCTIONS
-	
-	const saveToStorage = (state) => {
-		sessionStorage.setItem("state",JSON.stringify(state));
-	}
+	const {isLogged,error,loading} = useAppState();
+	const {getList} = useAction();
 	
 	useEffect(() => {
-		if(sessionStorage.getItem("state")) {
-			let state = JSON.parse(sessionStorage.getItem("state"));
-			if(state.isLogged) {
-				getList(state.token);
-			}
-			setState(state);
+		if(isLogged) {
+			getList();
 		}
-	},[]);
-	
-	//STATE FUNCTION
-	
-	const setError = (error) => {
-		setState((state) => {
-			let tempState = {
-				...state,
-				loading:false,
-				error:error
-			}
-			saveToStorage(tempState);
-			return tempState;
-		})
-	}
-	
-	const setLoading = (loading) => {
-		setState((state) => {
-			return {
-				...state,
-				loading:loading,
-				error:""
-			}
-		})
-	}
-	
-	const clearState = () => {
-		let state = {
-			list:[],
-			loading:false,
-			error:"",
-			token:"",
-			isLogged:false
-		}
-		saveToStorage(state);
-		setState(state);
-	}
-	
-	//FETCH
-	
-	useEffect(() => {
-		
-		const fetchData = async () => {
-			if(!urlRequest.url) {
-				return;
-			}
-			setLoading(true);
-			const response = await fetch(urlRequest.url,urlRequest.request);
-			setLoading(false);
-			if(!response) {
-				console.log("Error fetching!");
-				return;
-			}
-			if(response.ok) {
-				switch(urlRequest.action) {
-					case "additem":
-						getList();
-						return;
-					case "getlist":
-						let data = await response.json();
-						if(data) {
-							setState((state) => {
-								let tempState = {
-									...state,
-									list:data
-								}
-								saveToStorage(tempState);
-								return tempState;
-							})
-						}
-						return;
-					case "removeitem":
-						getList();
-						return;
-					case "edititem":
-						getList();
-						return;
-					case "register":
-						setError("Register success!");
-						return;
-					case "login":
-						let loginData = await response.json();
-						if(loginData) {
-							setState((state) => {
-								let tempState = {
-									...state,
-									isLogged:true,
-									token:loginData.token
-								}
-								saveToStorage(tempState);
-								return tempState;
-							})
-							getList(loginData.token);
-						}
-						return;
-					case "logout":
-						clearState();
-						return;
-					default:
-						return;
-				}
-			} else {
-				if(response.status === 403) {
-					clearState();
-					setError("Your session has expired. Please login again!");
-					return;
-				}
-				switch(urlRequest.action) {
-					case "additem":
-						setError("Error in adding new item. Server responded with a status"+response.status+" "+response.statusText);
-						return;
-					case "getlist":
-						setError("Error in fetching data. Server responded with a status"+response.status+" "+response.statusText)
-						return;
-					case "removeitem":
-						setError("Error in removing item. Server responded with a status"+response.status+" "+response.statusText)
-						return;
-					case "edititem":
-						setError("Error in editing item. Server responded with a status"+response.status+" "+response.statusText)
-						return;
-					case "register":
-						if(response.status === 409) {
-							setError("Username already in use");
-							return;
-						}
-						setError("Error when registering. Server responded with a status "+response.status+" "+response.statusText)
-						return;
-					case "login":
-						setError("Error logging in. Server responded with a status "+response.status+" "+response.statusText);
-						return;
-					case "logout":
-						clearState();
-						setError("Server responded with an error. Logging you out!");
-						return;
-					default:
-						return;
-				}
-			}
-		}
-		
-		fetchData();
-		
-	},[urlRequest])
-	
-	//LOGIN API
-	
-	const register = (user) => {
-		setUrlRequest({
-			url:"/register",
-			request:{
-				method:"POST",
-				headers:{"Content-Type":"application/json"},
-				body:JSON.stringify(user)
-			},
-			action:"register"
-		})
-	}
-	
-	const login = (user) => {
-		setUrlRequest({
-			url:"/login",
-			request:{
-				method:"POST",
-				headers:{"Content-Type":"application/json"},
-				body:JSON.stringify(user)
-			},
-			action:"login"
-		})
-	}
-	
-	const logout = () => {
-		setUrlRequest({
-			url:"/logout",
-			request:{
-				method:"POST",
-				headers:{"Content-Type":"application/json",
-				"token":state.token}
-			},
-			action:"logout"
-		})
-	}
-	
-	//SHOPPING API
-	
-	const getList = (token = state.token,search) => {
-		let url = "/api/shopping"
-		if(search) {
-		   url = "/api/shopping"+search
-		}
-		setUrlRequest({
-			url:url,
-			request:{
-				method:"GET",
-				headers:{"Content-Type":"application/json",
-						"token":token}
-			},
-			action:"getlist"
-		})
-	}
-	
-	const addItem = (item) => {
-		setUrlRequest({
-			url:"/api/shopping",
-			request:{
-				method:"POST",
-				headers:{"Content-Type":"application/json",
-						"token":state.token},
-				body:JSON.stringify(item)
-			},
-			action:"additem"
-		})
-	}
-	
-	const removeItem = (id) => {
-		setUrlRequest({
-			url:"/api/shopping/"+id,
-			request:{
-				method:"DELETE",
-				headers:{"Content-Type":"application/json",
-						"token":state.token}
-			},
-			action:"removeitem"
-		})
-	}
-	
-	const editItem = (item) => {
-		setUrlRequest({
-			url:"/api/shopping/"+item.id,
-			request:{
-				method:"PUT",
-				headers:{"Content-Type":"application/json",
-						"token":state.token},
-				body:JSON.stringify(item)
-			},
-			action:"edititem"
-		})
-	}
+	},[isLogged])
 	
 	//CONDITIONAL RENDERING
 	
 	let messageArea = <h4> </h4>
-	if(state.loading) {
+	if(loading) {
 		messageArea = <h4>Loading...</h4>
 	}
-	if(state.error) {
-		messageArea = <h4>{state.error}</h4>
+	if(error) {
+		messageArea = <h4>{error}</h4>
 	}
 	let routes = <Routes>
-				<Route exact path="/" element={<LoginPage login={login} register={register} setError={setError}/>}/>
+				<Route exact path="/" element={<LoginPage/>}/>
 				<Route path="*" element={<Navigate to="/"/>}/>
 				</Routes>
-	if(state.isLogged) {
+	if(isLogged) {
 		routes = <Routes>
-				<Route exact path="/" element={<ShoppingList list={state.list} removeItem={removeItem} editItem={editItem} getList={getList} token={state.token}/>}/>
-				<Route path="/form" element={<ShoppingForm addItem={addItem}/>}/>
+				<Route exact path="/" element={<ShoppingList/>}/>
+				<Route path="/form" element={<ShoppingForm/>}/>
 				<Route path="*" element={<Navigate to="/"/>}/>
 			</Routes>
 	}
 	return (
 		<div className="App">
-			<Navbar logout={logout} isLogged={state.isLogged}/>
+			<Navbar/>
 			{messageArea}
 			<hr/>
 			{routes}
---
+++ react/50_shopping_app/05_stage_statemanager/frontend/src/hooks/useAction.js
@@ -0,0 +1,261 @@
+import {useState,useEffect,useContext} from 'react';
+import ActionContext from '../context/ActionContext';
+import * as actionConstants from '../context/actionConstants';
+import useAppState from './useAppState';
+
+const useAction = () => {
+	
+	const action = useContext(ActionContext);
+	const [state,setState] = useState({
+		url:"",
+		request:{},
+		action:""
+	});
+	
+	const {token} = useAppState();
+	
+	useEffect(() => {
+		
+		const contactBackend = async () => {
+			if(!state.url) {
+				return;
+			}
+			action.dispatch({
+				type:actionConstants.LOADING
+			})
+			let response = await fetch(state.url,state.request);
+			action.dispatch({
+				type:actionConstants.STOP_LOADING
+			})
+			if(!response) {
+				action.dispatch({
+					type:actionConstants.LOGOUT_FAILED,
+					error:"There was no response from the server. Logging you out."
+				})
+			}
+			if(response.ok) {
+				switch(state.action) {
+					case "register":
+						action.dispatch({
+							type:actionConstants.REGISTER_SUCCESS
+						})
+						return;
+					case "login":
+						let data = await response.json();
+						if(!data) {
+							action.dispatch({
+								type:actionConstants.LOGIN_FAILED,
+								error:"Cannot parse login information. Login failed."
+							})
+							return;
+						}
+						action.dispatch({
+							type:actionConstants.LOGIN_SUCCESS,
+							token:data.token
+						})
+						return;
+					case "logout":
+						action.dispatch({
+							type:actionConstants.LOGOUT_SUCCESS
+						})
+						return;
+					case "getlist":
+						let list = await response.json();
+						if(!list) {
+							action.dispatch({
+								type:actionConstants.FETCH_LIST_FAILED,
+								error:"Failed to parse shopping information. Try again later."
+							})
+							return;
+						}
+						action.dispatch({
+							type:actionConstants.FETCH_LIST_SUCCESS,
+							list:list
+						})
+						return;
+					case "additem":
+						action.dispatch({
+							type:actionConstants.ADD_ITEM_SUCCESS
+						})
+						getList();
+						return;
+					case "removeitem":
+						action.dispatch({
+							type:actionConstants.REMOVE_ITEM_SUCCESS
+						})
+						getList();
+						return;
+					case "edititem":
+						action.dispatch({
+							type:actionConstants.EDIT_ITEM_SUCCESS
+						})
+						getList();
+						return;
+					default:
+						return;
+				}
+			} else {
+				if(response.status === 403) {
+					action.dispatch({
+						type:actionConstants.LOGOUT_FAILED,
+						error:"Your session has expired. Logging you out."
+					})
+					return;
+				}
+				let errorStatus = "Server responded with a status "+response.status+" "+response.statusText
+				switch(state.action) {
+					case "register":
+						if(response.status === 409) {
+							action.dispatch({
+								type:actionConstants.REGISTER_FAILED,
+								error:"Username already in use."
+							})
+							return;
+						}
+						action.dispatch({
+							type:actionConstants.REGISTER_FAILED,
+							error:"Register failed. "+errorStatus
+						});
+						return;
+					case "login":
+						action.dispatch({
+							type:actionConstants.LOGIN_FAILED,
+							error:"Login failed. "+errorStatus
+						})
+						return;
+					case "logout":
+						action.dispatch({
+							type:actionConstants.LOGOUT_FAILED,
+							error:"Server responded with an error. Logging you out."
+						})
+						return;
+					case "getlist":
+						action.dispatch({
+							type:actionConstants.FETCH_LIST_FAILED,
+							error:"Failed to fetch shopping information. "+errorStatus
+						})
+						return;
+					case "additem":
+						action.dispatch({
+							type:actionConstants.ADD_ITEM_FAILED,
+							error:"Adding an item failed. "+errorStatus
+						})
+						return;
+					case "removeitem":
+						action.dispatch({
+							type:actionConstants.REMOVE_ITEM_FAILED,
+							error:"Removing an item failed. "+errorStatus
+						})
+						return;
+					case "edititem":
+						action.dispatch({
+							type:actionConstants.EDIT_ITEM_FAILED,
+							error:"Editing an item failed. "+errorStatus
+						})
+						return;
+					default:
+						return;
+				}
+			}
+		}
+		
+		contactBackend();
+	},[state])
+	
+	const register = (user) => {
+		setState({
+			url:"/register",
+			request:{
+				method:"POST",
+				headers:{"Content-Type":"application/json"},
+				body:JSON.stringify(user)
+			},
+			action:"register"
+		})
+	}
+	
+	const login = (user) => {
+		setState({
+			url:"/login",
+			request:{
+				method:"POST",
+				headers:{"Content-Type":"application/json"},
+				body:JSON.stringify(user)
+			},
+			action:"login"
+		})
+	}	
+
+	const logout = () => {
+		setState({
+			url:"/logout",
+			request:{
+				method:"POST",
+				headers:{"Content-Type":"application/json",
+				"token":token}
+			},
+			action:"logout"
+		})
+	}
+	
+	const getList = () => {
+		setState({
+			url:"/api/shopping",
+			request:{
+				method:"GET",
+				headers:{"Content-Type":"application/json",
+				"token":token}
+			},
+			action:"getlist"
+		})
+	}
+	
+	const add = (item) => {
+			setState({
+			url:"/api/shopping",
+			request:{
+				method:"POST",
+				headers:{"Content-Type":"application/json",
+				"token":token},
+				body:JSON.stringify(item)
+			},
+			action:"additem"
+		})	
+	}
+
+	const remove = (id) =>  {
+		setState({
+			url:"/api/shopping/"+id,
+			request:{
+				method:"DELETE",
+				headers:{"Content-Type":"application/json",
+				"token":token}
+			},
+			action:"removeitem"
+		})		
+	}
+
+	const edit = (item) => {
+		setState({
+			url:"/api/shopping/"+item.id,
+			request:{
+				method:"PUT",
+				headers:{"Content-Type":"application/json",
+				"token":token},
+				body:JSON.stringify(item)
+			},
+			action:"edititem"
+		})
+	}
+	
+	const setError = (error) => {
+		action.dispatch({
+			type:actionConstants.REGISTER_FAILED,
+			error:error
+		})
+	}
+
+	return {register,login,logout,getList,add,remove,edit,setError}
+}
+
+export default useAction;
