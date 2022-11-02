ab65519a805fd2cc4cd9f0e513d19349958d5eb2
Erno Hentonen <erno.hentonen@opiframe.com>
2022-11-02 12:42:47 +0200

    typescript

---
+++ react/60_typescript/01_helloworld/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<HelloWorld/>
+		<HelloWorld name="Erno"/>
+    </div>
+  );
+}
+
+export default App;
