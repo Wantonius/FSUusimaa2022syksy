48f5fa1175ab9be44a304968fe3ebd66055e4767
Erno Hentonen <erno.hentonen@opiframe.com>
2022-11-08 09:03:01 +0200

    shoppingapp typescript redux

--- react/60_typescript/06_shopping_app_with_redux/src/App.tsx
+++ react/60_typescript/06_shopping_app_with_redux/src/App.tsx
@@ -2,13 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/LoginPage';
-
+import Navbar from './components/Navbar';
+import ShoppingForm from './components/ShoppingForm';
+import ShoppingList from './components/ShoppingList';
+import {Routes,Route,Navigate} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {useEffect} from 'react';
+import {AppState} from './types/states';
+import {getList} from './actions/shoppingActions';
+import {useDispatch} from 'react-redux';
 function App() {
-  return (
-    <div className="App">
-		<LoginPage/>
-    </div>
-  );
+	
+	const stateSelector = (state:AppState) => state;
+	
+	const dispatch = useDispatch();
+	
+	const state = useSelector(stateSelector);
+	
+	useEffect(() => {
+		if(state.login.isLogged) {
+			dispatch(getList(state.login.token));
+		}
+	},[state.login.isLogged])
+	
+	if(state.login.isLogged) {
+		return(
+		<div className="App">
+			<Navbar/>
+			<hr/>
+			<Routes>
+				<Route path="/" element={<ShoppingList/>}/>
+				<Route path="/form" element={<ShoppingForm/>}/>
+				<Route path="*" element={<Navigate to="/"/>}/>
+			</Routes>
+		</div>
+		)
+	} else {
+	return (
+		<div className="App">
+			<Navbar/>
+			<hr/>
+			<Routes>
+				<Route path="/" element={<LoginPage/>}/>
+				<Route path="*" element={<Navigate to="/"/>}/>
+			</Routes>
+		</div>
+	);
+	}
 }
 
 export default App;
--- react/60_typescript/06_shopping_app_with_redux/src/models/ShoppingItem.tsx
+++ react/60_typescript/06_shopping_app_with_redux/src/models/ShoppingItem.tsx
@@ -1,15 +1,14 @@
 export default class ShoppingItem {
 
-	id:number = 0;
+	id:number|string;
 	type:string = "";
 	count:number = 0;
 	price:number = 0;
 	
-	constructor(type:string,count:number,price:number,id:number) {
+	constructor(id:number|string,type:string,count:number,price:number) {
 		this.id = id;
 		this.type = type;
 		this.count = count;
 		this.price = price;
 	}
-	
 }
