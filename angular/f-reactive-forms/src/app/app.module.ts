290ef1581f7c12d200ce07b8e592c08e939bbb1c
Erno Hentonen <erno.hentonen@opiframe.com>
2022-11-08 13:35:21 +0200

    reactive forms

--- angular/f-reactive-forms/src/app/app.module.ts
+++ angular/f-reactive-forms/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ReactiveForm} from './reactiveform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	ReactiveForm
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
